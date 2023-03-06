8e67e03ee70402b48d7b71de60f4af36dfb62290
Rodrigo Iván Miranda Reidenbach <rodrigomreidenbach@gmail.com>
2023-03-06 10:26:33 -0300

    the bot is ready for deployment

--- src/bot.ts
+++ src/bot.ts
@@ -4,42 +4,45 @@ import { Telegraf, Context } from 'telegraf';
 import { UserGateway } from './dataAccess';
 import {CONFIG} from './config'
 
-export class Bot {
+
+export class Bot 
+{
+  
   private bot: Telegraf<Context>;
   private userGateway: UserGateway;
-  public phoneLost:boolean;
+  private sendAlerts: boolean;
 
   constructor(userGateway: UserGateway) {
     this.userGateway = userGateway;
     this.bot = new Telegraf(CONFIG.TELEGRAM_TOKEN);
-    this.phoneLost=false
-    this.bot.help((ctx) => ctx.reply('Send me a sticker'));
-    this.bot.hears('stop', (ctx) =>{
-        this.phoneLost=false
-        ctx.reply('you found your phone 👍')
-
-        }) ;
-    //funcion que escucha los mensajes de la gente
-    this.bot.hears(/^[0-9]{10,11}$/,async (ctx) => {
-      const chatId = ctx.chat?.id.toString();
-      if (chatId) {
-        const username = ctx.message?.from?.username;
-        if (username) {
-          const user = new User(username,chatId);
-          let serverResponse=await this.userGateway.insertData(user);
-          if (serverResponse == true){
-              ctx.reply(`Hi ${username}, your phone has been successfully registered`);
-          }else{
-              ctx.reply(`Hi ${username}, your phone has NOT been successfully registered. please try again later`);
-          }
-          
-        }
-      }
-    });
+    this.sendAlerts = true;
+    this.iniciar()
   }
 
-  iniciar(): void {
-    this.bot.launch();
+ private async iniciar(): Promise<void> {
+      //funcion que escucha los mensajes de la gente
+      this.bot.hears(/^[0-9]{10,11}$/,async (ctx) => {
+        const chatId = ctx.chat?.id.toString();
+        if (chatId) {
+          const username = ctx.message?.from?.username;
+          if (username) {
+            const user = new User(username,chatId);
+            let serverResponse=await this.userGateway.insertData(user);
+            if (serverResponse == true){
+                ctx.reply(`Hi ${username}, your phone has been successfully registered`);
+            }else{
+                ctx.reply(`Hi ${username}, your phone has NOT been successfully registered. please try again later`);
+            }
+            
+          }
+        }
+      });
+       this.bot.hears('stop', (ctx) => {
+        ctx.reply('you found your phone 👍');
+        this.sendAlerts = false;       
+      });
+    await this.bot.launch();
+
   }
 
   detener(): void {
@@ -47,23 +50,32 @@ export class Bot {
   }
 
   async enviarMensajeDeAlerta(username: string): Promise<void> {
-    console.log("BOT:enviarMensajeDeAlerta Activated")
-    try {
+    
+    try{
+      this.sendAlerts=true
       let chatId = await this.userGateway.getUserData(username);
-      if (chatId == null) {
-        console.log('chatId es null:',chatId)
-      } else {
-        console.log('BOT: chatid is ' + chatId)
-        const message = `Hi ${username}, your phone is here, type 'stop' to stop receiving this message`;
-        //i think that the problem is nearby these lines
-        this.bot.telegram.sendMessage(chatId, message);
-      }
+        if (chatId == null) {
+          console.log('chatId es null:', chatId)
+        
+        }else{        
+          const checkSendAlerts = setInterval(() => {
+            const message = `Hi ${username}, your phone is here, type 'stop' to stop receiving this message`
+            if(chatId){
+                this.bot.telegram.sendMessage(chatId, message);
+            }
+            if(!this.sendAlerts){
+                clearInterval(checkSendAlerts);   
+            }                   
+            }, 1000);
+         }
+       
+
     } catch (error) {
       console.log(error)
     }
-   
   }
-}
+} 
+   
 
 
 
